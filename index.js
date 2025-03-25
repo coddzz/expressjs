bcf5cb7bde4348f8729ddc163d74b15d19d6c124
Sarath <learn.cypher@gmail.com>
2025-03-26 01:27:43 +0530

    views

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req, res)=>{
+app.get('^/$|/index(.html)?',(req, res)=>{
     res.sendFile(path.join(__dirname,'views','index.html'))
 })
 
-app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`))
+app.get('/new-page(.html)?',(req, res)=>{
+    res.sendFile(path.join(__dirname,'views','new-page.html'))
+})
+
+app.get('/old-page(.html)?',(req, res) => {
+    res.redirect(301,'new-page.html');
+})
+
+app.get('/*',(req, res)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+})
+
+app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`));
