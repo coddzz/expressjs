c925ef3d80277cfa314cdfbde3f0a93990f64ca6
Sarath <learn.cypher@gmail.com>
2025-03-26 01:54:14 +0530

    next() function/Chain method

--- index.js
+++ index.js
@@ -15,8 +15,35 @@ app.get('/old-page(.html)?',(req, res) => {
     res.redirect(301,'new-page.html');
 })
 
+
+// req,res,next function
+app.get('/demo(.html)?',(req, res, next)=>{
+    console.log("demo page ")
+    next()
+},(req, res)=>{
+    res.send("hello world!!")
+})
+
+// Chain Method
+const one = (req, res, next) =>{
+    console.log("one")
+    next()
+}
+const two = (req, res, next) =>{
+    console.log("two")
+    next()
+}
+const three = (req, res) =>{
+    console.log("three")
+    res.send("Finished!!")
+}
+app.get('/chain(.html)?',[one,two,three]);
+
+
 app.get('/*',(req, res)=>{
     res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
+
+
 
 app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`));
