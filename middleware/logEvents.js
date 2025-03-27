c59f41ccc4f819aede99bf6157b8b6d60dd02eb0
Sarath <learn.cypher@gmail.com>
2025-03-27 22:21:50 +0530

    logEvents()

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -5,23 +5,18 @@ const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
 
-console.log(`${format(new Date(), 'dd-MM-yyyy\tHH:mm:ss')}`)
-
-console.log(uuid())
-
 const logEvents = async (message) => {
     const datetime = `${format(new Date(), 'dd-MM-yyyy\tHH:mm:ss')}`
     const logItem = `${datetime} \t ${uuid()} \t ${message}\n`
     console.log(logItem);
     try{
-        if(!fs.existsSync(path.join(__dirname,'logs'))){
-            await fsPromises.mkdir(path.join(__dirname,'logs'))
+        if(!fs.existsSync(path.join(__dirname,'..','logs'))){
+            await fsPromises.mkdir(path.join(__dirname,'..','logs'))
         }
-        await fsPromises.appendFile(path.join(__dirname,'logs','logFile.txt'),logItem)
+        await fsPromises.appendFile(path.join(__dirname,'..','logs','logFile.txt'),logItem)
     } catch (err){
         console.log(err)
     }
 }
 
 logEvents()
-    
