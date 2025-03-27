169fe6eb1609b2ef609e5b0f3643e9ad3133dee0
Sarath <learn.cypher@gmail.com>
2025-03-27 22:14:47 +0530

    logEvents()

--- logEvents.js
+++ logEvents.js
@@ -1,6 +1,7 @@
 const {format} = require('date-fns')
 const {v4:uuid} = require('uuid')
 
+const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
 
@@ -12,6 +13,15 @@ const logEvents = async (message) => {
     const datetime = `${format(new Date(), 'dd-MM-yyyy\tHH:mm:ss')}`
     const logItem = `${datetime} \t ${uuid()} \t ${message}\n`
     console.log(logItem);
+    try{
+        if(!fs.existsSync(path.join(__dirname,'logs'))){
+            await fsPromises.mkdir(path.join(__dirname,'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname,'logs','logFile.txt'),logItem)
+    } catch (err){
+        console.log(err)
+    }
 }
 
 logEvents()
+    
